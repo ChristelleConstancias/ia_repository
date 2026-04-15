a355fbf7ffcd78749d52d3f91e19444744e8b761
ChristelleConstancias <140883602+ChristelleConstancias@users.noreply.github.com>
2026-04-15 17:25:06 +0200

    Calculate and display sales volume statistics
    Added code to calculate and print sales volume statistics by product.

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ import pandas as pd
 
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
+stats_volume = (
+    données
+    .groupby('produit')['qte']
+    .agg(
+        moyenne_volume='mean',
+        mediane_volume='median'
+    )
+    .reset_index()
+)
+
+print("📦 Volume des ventes par produit")
+print(stats_volume)
+
+
 figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
 
 figure.write_html('ventes-par-region.html')
